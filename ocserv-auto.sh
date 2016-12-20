6365ef2f1b18b8216c21b9c60d27712c83aab216
jimorsm <jimorsm@gmail.com>
2016-12-20 22:00:35 +0200

    fix

--- ocserv-auto.sh
+++ ocserv-auto.sh
@@ -213,27 +213,29 @@ _EOF_
     #锐速加速设置
 
     if [ -f "$lotServer" ]; then
+        touch ${confdir}/lotServerreload.sh
+        chmod +x ${confdir}/lotServerreload.sh
+        sed -i 's@^#connect-script.*@connect-script = ${confdir}/lotServerreload.sh@g' "${confdir}/lotServerreload.sh"
+        sed -i 's@^#disconnect-script.*@connect-script = ${confdir}/lotServerreload.sh@g' "${confdir}/lotServerreload.sh"
         cat << _EOF_ >>${confdir}/lotServerreload.sh 
         #!/bin/sh
         wanif=$(ip a|grep vpns|grep inet|awk '{print $NF}') 
         sed -i "s/^accif=.*\$/accif=\"eth0 $(echo $wanif)\"/" /appex/etc/config
         /appex/bin/lotServer.sh reload
         _EOF_
-        chmod +x ${confdir}/lotServerreload.sh
-        sed -i 's@^#connect-script.*@connect-script = ${confdir}/lotServerreload.sh@g' "${confdir}/lotServerreload.sh"
-        sed -i 's@^#disconnect-script.*@connect-script = ${confdir}/lotServerreload.sh@g' "${confdir}/lotServerreload.sh"
     fi
 
     if [ -f "$serverSpeeder" ]; then
+        touch ${confdir}/serverSpeederreload.sh
+        chmod +x ${confdir}/serverSpeederreload.sh
+        sed -i 's@^#connect-script.*@connect-script = ${confdir}/serverSpeederreload.sh@g' "${confdir}/serverSpeederreload.sh"
+        sed -i 's@^#disconnect-script.*@connect-script = ${confdir}/serverSpeederreload.sh@g' "${confdir}/serverSpeederreload.sh"
         cat << _EOF_ >>${confdir}/serverSpeederreload.sh
         #!/bin/sh
         wanif=$(ip a|grep vpns|grep inet|awk '{print $NF}')
         sed -i "s/^accif=.*\$/accif=\"eth0 $(echo $wanif)\"/" /serverspeeder/etc/config
         /serverspeeder/bin/serverSpeeder.sh reload
         _EOF_
-        chmod +x ${confdir}/serverSpeederreload.sh
-        sed -i 's@^#connect-script.*@connect-script = ${confdir}/serverSpeederreload.sh@g' "${confdir}/serverSpeederreload.sh"
-        sed -i 's@^#disconnect-script.*@connect-script = ${confdir}/serverSpeederreload.sh@g' "${confdir}/serverSpeederreload.sh"
     fi
 
 #     cat << _EOF_ >>${confdir}/ocserv.conf
